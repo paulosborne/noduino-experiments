a9e843c2aba4975f838354e287a7581da4f66e9d
Paul Osborne <paul.osborne.uk@gmail.com>
2013-11-29 07:26:40 +0000

    Initial commit

---
+++ test1.js
@@ -0,0 +1,23 @@
+var arduino = require('duino'),
+    _       = require('underscore');
+
+var pins    = [3,4,5,6,7,8];
+var leds    = {};
+var ticks   = 100;
+
+var board = new arduino.Board({
+    device : "ACM",
+    debug  : true
+});
+
+board.on('ready', function () {
+
+    pins.forEach(function (num) {
+        leds[num] = new arduino.Led({ board: board, pin: num })
+    });
+
+    _.each(_.keys(leds), function (pin) {
+        console.log(pin);
+        _.throttle(leds[pin])
+    });
+});
